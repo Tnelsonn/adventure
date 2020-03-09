a15805216b39caf2a222b7e783e93b53856d632d
Tnelsonn <topher4403@gmail.com>
2020-03-09 08:45:22 -0500

    rEEE

--- MinecraftAdventure.py
+++ MinecraftAdventure.py
@@ -16,12 +16,21 @@ def Forest():
     if collection == "explore":
         print("you find a village but you are tired and hungry from all the running")
         ###add class for villagers and allow person to choose who to talk to###
+
     else: woodtool = input("you chopped down a tree. What would you like to do with the wood? Would you like to create a sword or a pickaxe?")
-    if woodtool == "sword":
-        print(" You make a crafting table and make a wood sword, but you remember you have a rusted one already " )
-    else: cave1 = input(" You built your pickaxe, and you remember you saw a mine when you were breaking the tree down. Will you go mining? ")
+
+    if woodtool == "sword": cave1 = input("you craft a wood sword and realize that its worse than the rusty one you have. So, you get more wood and make a pickaxe. Would you like to go mining?")
+
+    if: cave1 = input(" You built your pickaxe, and you remember you saw a mine when you were breaking the tree down. Will you go mining? ")
+
+    else: buildhome = input("you decide to not go mining, instead you gather materials and food for later.") 
+
     if cave1 == "yes": deepcave =input("you got some cobble stone and made a better pickaxe, would you like to go further?")
-    if deepcave1 == "yes": print("You go further and find some iron to mine. its a dead end and you leave the cave")
+
+    if deepcave == "yes": print("You go further and find some iron to mine. its a dead end and you leave the cave")
     buildhome = input(" You finished mining and killed a chicken for some food would you like to build a house and sleep?" )
+
+    if buildhome == "yes": print( "You built a small house and used the wool from a sheep to make a small bed, You decide its time for bed and got to sleep")
+    else: 
 Forest()
 
