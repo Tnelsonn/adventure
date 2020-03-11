d7f291b2cf3fdcda359ab7ee15464bf3bac1c445
Tnelsonn <topher4403@gmail.com>
2020-03-11 08:22:28 -0500

    Part %

--- MinecraftAdventure.py
+++ MinecraftAdventure.py
@@ -11,17 +11,12 @@ def intro():
 intro()
 
 def Forest():
-    collection = input("What will you do now that the man is gone? Will you explore or gather resources?")
-
-    if collection == "explore":
-        print("you find a village but you are tired and hungry from all the running")
-        ###add class for villagers and allow person to choose who to talk to###
-
-    else: woodtool = input("you chopped down a tree. What would you like to do with the wood? Would you like to create a sword or a pickaxe?")
+    print(" After the man leaves you decide to gather resources")
+    woodtool = input("you chopped down a tree. What would you like to do with the wood? Would you like to create a sword or a pickaxe?")
 
     if woodtool == "sword": cave1 = input("you craft a wood sword and realize that its worse than the rusty one you have. So, you get more wood and make a pickaxe. Would you like to go mining?")
 
-    else: woodtool == input(" You built your pickaxe, and you remember you saw a mine when you were breaking the tree down. Will you go mining? ")
+    else woodtool:  == input(" You built your pickaxe, and you remember you saw a mine when you were breaking the tree down. Will you go mining? ")
 
     if cave1 == "yes": deepcave =input("you got some cobble stone and made a better pickaxe, would you like to go further?")
 
@@ -34,6 +29,8 @@ def Forest():
     if buildhome == "yes": print( "You built a small house and used the wool from a sheep to make a small bed, You decide its time for bed and got to sleep")
 
     else: print( "Oof, It looks like you got ambushed by a creeper! Better luck next time")
-     
+    life1 = input(" respawn or quit? ")
+    if life1 == "respawn": print("respawn")
+    else: print(" Goodbye")
 Forest()
 
